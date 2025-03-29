41c6527f0d9370f34dbeba51348b667c0d1d800e
Volodymyr199606 <your-email@example.com>
2025-03-29 16:15:14 -0700

    implementing abstraction

--- src/main/java/org/com/models/Person.java
+++ src/main/java/org/com/models/Person.java
@@ -1,4 +1,22 @@
 package org.com.models;
 
-public class Person {
+public abstract class Person {
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public Person(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+
+    public String getContactInfo() {
+        return name + " (" + email + ")";
+    }
+
 }
