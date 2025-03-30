28c12b884b05301261d4a2d0064d4bce7c6de2ff
Volodymyr199606 <your-email@example.com>
2025-03-30 13:10:37 -0700

    Implementing logic in User

--- src/main/java/org/com/models/User.java
+++ src/main/java/org/com/models/User.java
@@ -1,4 +1,23 @@
 package org.com.models;
 
-public class User {
+public class User extends Person {
+    private int id;
+
+    public User(int id, String name, String email, String password) {
+        super(name, email, password);
+        this.id = id;
+
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+
+    @Override
+    public String toString() {
+        return "User{id=" + id +
+                ", contact='" + getContactInfo() + '\'' +
+                ", role='" + '\'' +
+                '}';
+    }
 }
