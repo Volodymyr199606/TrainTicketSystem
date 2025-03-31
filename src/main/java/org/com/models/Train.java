90e18db75cbc547188052707758f8c55069a68c9
Volodymyr199606 <your-email@example.com>
2025-03-31 16:45:09 -0700

    add constructor to the train

--- src/main/java/org/com/models/Train.java
+++ src/main/java/org/com/models/Train.java
@@ -1,4 +1,20 @@
 package org.com.models;
 
 public class Train {
+    private int id;
+    private String trainName;
+    private String departureTime;
+    private String arrivalTime;
+    private int distanceKm;
+    private int totalSeats;
+
+    // Constructor
+    public Train(int id, String trainName, String departureTime, String arrivalTime, int distanceKm, int totalSeats) {
+        this.id = id;
+        this.trainName = trainName;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.distanceKm = distanceKm;
+        this.totalSeats = totalSeats;
+    }
 }
