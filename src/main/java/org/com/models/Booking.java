e52a52d0bef1821017ddc3603b518ad170614a95
Volodymyr199606 <your-email@example.com>
2025-03-31 13:02:06 -0700

    add logic in Booking

--- src/main/java/org/com/models/Booking.java
+++ src/main/java/org/com/models/Booking.java
@@ -1,4 +1,23 @@
 package org.com.models;
 
 public class Booking {
+    private int id;
+    private int userId;
+    private int trainId;
+    private int seatsBooked;
+    private String bookingTime;
+
+    public Booking(int id, int userId, int trainId, int seatsBooked, String bookingTime) {
+        this.id = id;
+        this.userId = userId;
+        this.trainId = trainId;
+        this.seatsBooked = seatsBooked;
+        this.bookingTime = bookingTime;
+    }
+
+    public int getId() { return id; }
+    public int getUserId() { return userId; }
+    public int getTrainId() { return trainId; }
+    public int getSeatsBooked() { return seatsBooked; }
+    public String getBookingTime() { return bookingTime; }
 }
