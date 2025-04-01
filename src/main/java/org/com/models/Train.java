5c1ca1004060b6af88b5925cc0afedaf8f361a6c
Volodymyr199606 <your-email@example.com>
2025-04-01 10:22:29 -0700

    add getterts & setters in train

--- src/main/java/org/com/models/Train.java
+++ src/main/java/org/com/models/Train.java
@@ -8,7 +8,7 @@ public class Train {
     private int distanceKm;
     private int totalSeats;
 
-    // Constructor
+
     public Train(int id, String trainName, String departureTime, String arrivalTime, int distanceKm, int totalSeats) {
         this.id = id;
         this.trainName = trainName;
@@ -16,5 +16,57 @@ public class Train {
         this.arrivalTime = arrivalTime;
         this.distanceKm = distanceKm;
         this.totalSeats = totalSeats;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTrainName() {
+        return trainName;
+    }
+
+    public void setTrainName(String trainName) {
+        this.trainName = trainName;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getDistanceKm() {
+        return distanceKm;
+    }
+
+    public void setDistanceKm(int distanceKm) {
+        this.distanceKm = distanceKm;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(int totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+
+    public String getName() {
+        return trainName;
     }
 }
