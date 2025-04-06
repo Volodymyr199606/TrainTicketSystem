e1336c327e17a3a9678fbe1a18e170b24bd9b221
Volodymyr199606 <your-email@example.com>
2025-04-05 22:53:02 -0700

    add service

--- src/main/java/org/com/services/TrainService.java
+++ src/main/java/org/com/services/TrainService.java
@@ -1,4 +1,24 @@
 package org.com.services;
 
+import org.com.dao.TrainDAO;
+import org.com.models.Train;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class TrainService {
+    private final TrainDAO trainDAO;
+
+    public TrainService(Connection conn) {
+        this.trainDAO = new TrainDAO(conn);
+    }
+
+    public boolean registerTrain(Train train) throws SQLException {
+        return trainDAO.addTrain(train);
+    }
+
+    public List<Train> getAllTrains() throws SQLException {
+        return trainDAO.getTrains();
+    }
 }
