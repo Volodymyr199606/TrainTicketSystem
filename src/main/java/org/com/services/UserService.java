22a1884e2d5d2dcb6d03f808b27aaa7dd1855375
Volodymyr199606 <your-email@example.com>
2025-04-08 14:52:19 -0700

    implement logic

--- src/main/java/org/com/services/UserService.java
+++ src/main/java/org/com/services/UserService.java
@@ -1,4 +1,30 @@
 package org.com.services;
 
-public class UserService {
+import org.com.dao.UserDAO;
+import org.com.models.User;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class UserService implements UserServiceInterface {
+    private final UserDAO userDAO;
+
+    public UserService(Connection conn) {
+        this.userDAO = new UserDAO(conn);
+    }
+
+    @Override
+    public boolean registerUser(User user) {
+        try {
+            return userDAO.registerUser(user);
+        } catch (SQLException e) {
+            System.out.println("Error registering user: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public User loginUser(String email, String password) {
+        return userDAO.loginUser(email, password);
+    }
 }
