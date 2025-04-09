f2171289710f0f1733835d31c132b6fb2979f731
Volodymyr199606 <your-email@example.com>
2025-04-09 10:54:37 -0700

    update main

--- src/main/java/org/com/Main.java
+++ src/main/java/org/com/Main.java
@@ -1,13 +1,23 @@
 package org.com;
 
 import org.com.database.DatabaseConnection;
-
+import org.com.services.BookingService;
+import org.com.services.TrainService;
+import org.com.services.UserService;
+import org.com.services.UserServiceInterface;
+import org.com.ui.TrainBookingUI;
 
 import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
         try (Connection conn = DatabaseConnection.getConnection()) {
+            UserServiceInterface userService = new UserService(conn);
+            TrainService trainService = new TrainService(conn);
+            BookingService bookingService = new BookingService(conn);
+
+            TrainBookingUI bookingUI = new TrainBookingUI(userService, trainService, bookingService);
+            bookingUI.startApp();
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
